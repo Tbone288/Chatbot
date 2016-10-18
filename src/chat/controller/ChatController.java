d889e0d56cc80893c35eebf937555f0ca1b58f33
Tbone288 <todo3858@740s-pl-1176.local>
2016-10-18 12:39:51 -0600

    Created ChatViewer class

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,21 @@
+package chat.controller;
+
+import chat.model.Chatbot;
+import chat.view.ChatViewer;
+
+public class ChatController
+{
+	private Chatbot stupidBot;
+	private ChatViewer chatView;
+	
+	public ChatController()
+	{
+		stupidBot = new Chatbot("Boaty McBoatFace");
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+}
