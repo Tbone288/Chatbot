d046fa60c08f65dd9f5e0c70d124220f0d02b950
Tbone288 <todo3858@740s-pl-1176.local>
2016-10-31 12:06:38 -0600

    chatbot now prints pile of poop emoji

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -104,6 +104,9 @@ public class Chatbot
 	{
 		boolean hasContent = false;
 		
+		String temp = "💩";
+		System.out.println(temp + temp.toLowerCase());
+		
 		if(currentInput.toLowerCase().contains(content.toLowerCase()))
 		{
 			hasContent = true;
